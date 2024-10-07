61553de235360425f344594cda194701de857da5
Ayoub Elazhari <a.elazhari0495@uca.ac.ma>
2024-10-07 10:56:42 +0100

    Libft

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,20 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 10:54:56 by ayoub             #+#    #+#             */
-/*   Updated: 2024/10/07 10:54:57 by ayoub            ###   ########.fr       */
+/*   Created: 2024/10/07 10:53:53 by ayoub             #+#    #+#             */
+/*   Updated: 2024/10/07 10:55:26 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_tolower(int c)
+int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    if (c >= 'A' && c <= 'Z')
-        c += 32;
-    return (c);
+    size_t      i;
+    unsigned char   *ptr1;
+    unsigned char   *ptr2;
+
+    i = 0;
+    ptr1 = (unsigned char *)s1;
+    ptr2 = (unsigned char *)s2;
+    while (i < n)
+    {
+        if (ptr1[i] != ptr2[i])
+            return (ptr1[i] - ptr2[i]);
+        i++;
+    }
+    return (0);
 }
--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 10:55:02 by ayoub             #+#    #+#             */
+/*   Updated: 2024/10/07 10:55:03 by ayoub            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 #define LIBFT_H
 
@@ -26,5 +38,6 @@ char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
