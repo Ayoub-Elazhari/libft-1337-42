7b95ef5481e6574bfb54578f80a1e3aaeb62e867
Ayoub Elazhari <a.elazhari0495@uca.ac.ma>
2024-10-07 14:29:17 +0100

    Libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 10:55:02 by ayoub             #+#    #+#             */
-/*   Updated: 2024/10/07 12:52:48 by ayoub            ###   ########.fr       */
+/*   Updated: 2024/10/07 14:00:07 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 char    *ft_strnstr(const char *big, const char *little, size_t len);
+void    ft_putchar_fd(int c, int fd);
+void    ft_putstr_fd(char *s, int fd);
 
 #endif
